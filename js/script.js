534e4640c22320f995eb8cf8e81952f3066b66c1
Francesco Fonte <webffranz@gmail.com>
2011-07-22 20:53:32 +0200

    calendar

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*  Your JavaScript for Resource  */
+
+$(document).ready(function(){
+
+	Utils.init();
+
+});
+
+var Utils  = {
+	
+	init : function(){
+	
+		$('.menutop').append(' &mdash; <a href="http://www.premiumpixels.com/freebies/pretty-little-calendar-psd/" target="_blank">Inspired by: Pixel Premium</a> ')
+	
+	}
+	
+}
